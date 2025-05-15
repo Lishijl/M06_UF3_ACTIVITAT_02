a806f3109d60383636fa914f9e5d0d2b72ebadf0
Christopherjfc <christopherjfc1@gmail.com>
2025-05-15 20:11:20 +0200

    package arreglado

--- apireactive/apireactive/src/main/java/com/iticbcn/apireactive/config/OpenApiConfig.java
+++ apireactive/apireactive/src/main/java/com/iticbcn/apireactive/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package main.java.com.iticbcn.apireactive.config;
+package com.iticbcn.apireactive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
