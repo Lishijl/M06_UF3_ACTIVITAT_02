89282bc5047781f0f03e00ff7952f110e38d840b
Lishi Ji Liu <liulishij@gmail.com>
2025-05-15 19:42:31 +0200

    esquelet i config class implementats

---
+++ apireactive/apireactive/src/main/java/com/iticbcn/apireactive/ApireactiveApplication.java
@@ -0,0 +1,13 @@
+package com.iticbcn.apireactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApireactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApireactiveApplication.class, args);
+	}
+
+}
---
+++ apireactive/apireactive/src/test/java/com/iticbcn/apireactive/ApireactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iticbcn.apireactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApireactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
