99d14caaa91e7400090f646851ef20885c1de4a6
Christopherjfc <christopherjfc1@gmail.com>
2025-05-15 20:09:44 +0200

    Document Student.java  y record StudentDTO creado

---
+++ apireactive/apireactive/src/main/java/com/iticbcn/apireactive/DTO/StudentDTO.java
@@ -0,0 +1,11 @@
+package com.iticbcn.apireactive.DTO;
+
+public record StudentDTO(
+        int id,
+        String name,
+        String surname,
+        String email,
+        String phone
+) {
+
+}
---
+++ apireactive/apireactive/src/main/java/com/iticbcn/apireactive/model/Student.java
@@ -0,0 +1,29 @@
+package com.iticbcn.apireactive.model;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document(collection = "students")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Student implements Serializable{
+    @Id
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+    private String phone;
+
+}
+
